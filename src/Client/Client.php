1f7bc8a50b1823dc4aa32c37c04e9375128d98a5
Finn Paes <finn@justbetter.nl>
2023-10-20 13:23:32 +0200

    Add new sendAsyncRequest method

--- src/Client/Client.php
+++ src/Client/Client.php
@@ -2,15 +2,39 @@
 
 namespace JustBetter\AkeneoClient\Client;
 
+use Akeneo\Pim\ApiClient\Client\ClientInterface;
+use GuzzleHttp\Promise\PromiseInterface;
+use Http\Promise\Promise;
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
-use Psr\Http\Client\ClientInterface;
+use JustBetter\AkeneoClient\Exceptions\AkeneoException;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 class Client implements ClientInterface
 {
     public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        return $this->buildHttpRequest($request)
+            ->send($request->getMethod(), $request->getUri())
+            ->toPsrResponse();
+    }
+
+    public function sendAsyncRequest(RequestInterface $request): PromiseInterface|Promise
+    {
+        $promise = $this->buildHttpRequest($request)
+            ->async()
+            ->getPromise();
+
+        if (is_null($promise)) {
+            throw new AkeneoException('Unable to send async request, empty promise given!');
+        }
+
+        return $promise;
+    }
+
+    protected function buildHttpRequest(RequestInterface $request): PendingRequest
     {
         $contentTypes = $request->getHeader('Content-Type');
 
@@ -29,8 +53,6 @@ class Client implements ClientInterface
         return Http::withHeaders($headers)
             ->timeout($timeout)
             ->connectTimeout($connectTimeout)
-            ->withBody((string) $request->getBody(), $contentType)
-            ->send($request->getMethod(), $request->getUri())
-            ->toPsrResponse();
+            ->withBody((string) $request->getBody(), $contentType);
     }
 }
