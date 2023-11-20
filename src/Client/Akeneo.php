301f95e30e3d3e53a45b17e3b0eedfcf0c9959c5
Ramon Rietdijk <ramon@justbetter.nl>
2023-11-20 15:17:49 +0100

    Updated doc blocks and introduced attributes

--- src/Client/Akeneo.php
+++ src/Client/Akeneo.php
@@ -3,24 +3,104 @@
 namespace JustBetter\AkeneoClient\Client;
 
 use Akeneo\Pim\ApiClient\AkeneoPimClientInterface;
+use Akeneo\Pim\ApiClient\Api\AppCatalog\AppCatalogApiInterface;
+use Akeneo\Pim\ApiClient\Api\AppCatalog\AppCatalogProductApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetCategoryApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetManager\AssetApiInterface as AssetManagerApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetManager\AssetAttributeApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetManager\AssetAttributeOptionApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetManager\AssetFamilyApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetManager\AssetMediaFileApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetReferenceFileApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetTagApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssetVariationFileApiInterface;
+use Akeneo\Pim\ApiClient\Api\AssociationTypeApiInterface;
+use Akeneo\Pim\ApiClient\Api\AttributeApiInterface;
+use Akeneo\Pim\ApiClient\Api\AttributeGroupApiInterface;
+use Akeneo\Pim\ApiClient\Api\AttributeOptionApiInterface;
+use Akeneo\Pim\ApiClient\Api\CategoryApiInterface;
+use Akeneo\Pim\ApiClient\Api\ChannelApiInterface;
+use Akeneo\Pim\ApiClient\Api\CurrencyApiInterface;
+use Akeneo\Pim\ApiClient\Api\FamilyApiInterface;
+use Akeneo\Pim\ApiClient\Api\FamilyVariantApiInterface;
+use Akeneo\Pim\ApiClient\Api\LocaleApiInterface;
+use Akeneo\Pim\ApiClient\Api\MeasureFamilyApiInterface;
+use Akeneo\Pim\ApiClient\Api\MeasurementFamilyApiInterface;
+use Akeneo\Pim\ApiClient\Api\MediaFileApiInterface;
+use Akeneo\Pim\ApiClient\Api\Operation\DownloadableResourceInterface;
+use Akeneo\Pim\ApiClient\Api\ProductApiInterface;
+use Akeneo\Pim\ApiClient\Api\ProductDraftApiInterface;
+use Akeneo\Pim\ApiClient\Api\ProductDraftUuidApiInterface;
+use Akeneo\Pim\ApiClient\Api\ProductModelApiInterface;
+use Akeneo\Pim\ApiClient\Api\ProductModelDraftApiInterface;
+use Akeneo\Pim\ApiClient\Api\ProductUuidApiInterface;
+use Akeneo\Pim\ApiClient\Api\PublishedProductApiInterface;
+use Akeneo\Pim\ApiClient\Api\ReferenceEntityApiInterface;
+use Akeneo\Pim\ApiClient\Api\ReferenceEntityAttributeApiInterface;
+use Akeneo\Pim\ApiClient\Api\ReferenceEntityAttributeOptionApiInterface;
+use Akeneo\Pim\ApiClient\Api\ReferenceEntityMediaFileApiInterface;
+use Akeneo\Pim\ApiClient\Api\ReferenceEntityRecordApiInterface;
 use Illuminate\Support\Facades\Http;
 use JustBetter\AkeneoClient\Exceptions\AkeneoException;
+use SensitiveParameter;
 
-/** @mixin AkeneoPimClientInterface */
+/**
+ * @method ?string getToken()
+ * @method ?string getRefreshToken()
+ * @method ProductApiInterface getProductApi()
+ * @method CategoryApiInterface getCategoryApi()
+ * @method DownloadableResourceInterface getCategoryMediaFileApi()
+ * @method AttributeApiInterface getAttributeApi()
+ * @method AttributeOptionApiInterface getAttributeOptionApi()
+ * @method AttributeGroupApiInterface getAttributeGroupApi()
+ * @method FamilyApiInterface getFamilyApi()
+ * @method MediaFileApiInterface getProductMediaFileApi()
+ * @method LocaleApiInterface getLocaleApi()
+ * @method ChannelApiInterface getChannelApi()
+ * @method CurrencyApiInterface getCurrencyApi()
+ * @method MeasureFamilyApiInterface getMeasureFamilyApi()
+ * @method MeasurementFamilyApiInterface getMeasurementFamilyApi()
+ * @method AssociationTypeApiInterface getAssociationTypeApi()
+ * @method FamilyVariantApiInterface getFamilyVariantApi()
+ * @method ProductModelApiInterface getProductModelApi()
+ * @method PublishedProductApiInterface getPublishedProductApi()
+ * @method ProductModelDraftApiInterface getProductModelDraftApi()
+ * @method ProductDraftApiInterface getProductDraftApi()
+ * @method AssetApiInterface getAssetApi()
+ * @method AssetCategoryApiInterface getAssetCategoryApi()
+ * @method AssetTagApiInterface getAssetTagApi()
+ * @method AssetReferenceFileApiInterface getAssetReferenceFileApi()
+ * @method AssetVariationFileApiInterface getAssetVariationFileApi()
+ * @method ReferenceEntityRecordApiInterface getReferenceEntityRecordApi()
+ * @method ReferenceEntityMediaFileApiInterface getReferenceEntityMediaFileApi()
+ * @method ReferenceEntityAttributeApiInterface getReferenceEntityAttributeApi()
+ * @method ReferenceEntityAttributeOptionApiInterface getReferenceEntityAttributeOptionApi()
+ * @method ReferenceEntityApiInterface getReferenceEntityApi()
+ * @method AssetManagerApiInterface getAssetManagerApi()
+ * @method AssetFamilyApiInterface getAssetFamilyApi()
+ * @method AssetAttributeApiInterface getAssetAttributeApi()
+ * @method AssetAttributeOptionApiInterface getAssetAttributeOptionApi()
+ * @method AssetMediaFileApiInterface getAssetMediaFileApi()
+ * @method ProductUuidApiInterface getProductUuidApi()
+ * @method ProductDraftUuidApiInterface getProductDraftUuidApi()
+ * @method AppCatalogApiInterface getAppCatalogApi()
+ * @method AppCatalogProductApiInterface getAppCatalogProductApi()
+ */
 class Akeneo
 {
     protected AkeneoPimClientInterface $client;
 
-    public function __construct(string $url, string $clientId, string $secret, string $username, string $password)
-    {
+    public function __construct(
+        string $url,
+        string $clientId,
+        #[SensitiveParameter] string $secret,
+        string $username,
+        #[SensitiveParameter] string $password
+    ) {
         $clientBuilder = new ClientBuilder($url);
 
-        $this->client = $clientBuilder->buildAuthenticatedByPassword(
-            clientId: $clientId,
-            secret: $secret,
-            username: $username,
-            password: $password
-        );
+        $this->client = $clientBuilder->buildAuthenticatedByPassword($clientId, $secret, $username, $password);
     }
 
     public function __call(string $method, array $args): mixed
